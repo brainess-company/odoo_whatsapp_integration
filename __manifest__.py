9ebe09fa1a0ed39d1bb0711145d3240f916d083b
BrainessCompany <brainesscompany@gmail.com>
2025-03-20 10:34:26 -0300

    removi a instalação do web

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'web', 'stock', 'purchase','account','contacts'],
+    'depends': ['base', 'sale', 'stock', 'purchase','account','contacts'],
 
     # always loaded
     'data': [
